465034e88ed2f13eec9b99ccadcada344e5a8275
Justin <DilksJustin@johndeere.com>
2022-10-04 10:15:34 -0500

    pass db password through env

--- src/util/db.js
+++ src/util/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export async function connect() {
     if(!mongoose.connection.readyState) {
-        await mongoose.connect("mongodb://localhost:27017/banking-app")
+        await mongoose.connect(`mongodb+srv://dilksjm:${process.env.MONGO_PASSWORD}@banking-app.fk8im0q.mongodb.net/?retryWrites=true&w=majority`)
     }
 }
 
