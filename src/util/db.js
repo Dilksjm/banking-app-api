5bc4a0cfcdd7e9a07e089cebfc95379f8d634d09
Justin <DilksJustin@johndeere.com>
2022-10-04 10:23:13 -0500

    include db name

--- src/util/db.js
+++ src/util/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export async function connect() {
     if(!mongoose.connection.readyState) {
-        await mongoose.connect(`mongodb+srv://dilksjm:${process.env.MONGO_PASSWORD}@banking-app.fk8im0q.mongodb.net/?retryWrites=true&w=majority`)
+        await mongoose.connect(`mongodb+srv://dilksjm:${process.env.MONGO_PASSWORD}@banking-app.fk8im0q.mongodb.net/banking-app?retryWrites=true&w=majority`)
     }
 }
 
